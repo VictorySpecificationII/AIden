d12e8aff197ca82b89b2ed24b4adf8085c77ca59
Antreas Christofi <victoryspecificationii@gmail.com>
2024-04-11 14:04:20 +0300

    added vm resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,22 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "aiden-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone = "europe-west4-a"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
