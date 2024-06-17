9c3f6739e46eee8794807070847550aa83f2f953
Antreas Christofi <victoryspecificationii@gmail.com>
2024-06-17 22:49:32 +0300

    clearned up setup.py file

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
-        'llama-index',
         'transformers',
         'torch',
         'requests',
         'pytest',
-        'python-dotenv',
-	'pip install llama-index-llms-huggingface',
-	'pip install llama-index-embeddings-huggingface'
+        'python-dotenv'
     ],
     entry_points={
         'console_scripts': [
