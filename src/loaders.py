1fd82b632a2a78fdb635ade4af7523c096aef0b0
Antreas Christofi <victoryspecificationii@gmail.com>
2024-06-17 21:07:08 +0300

    created csv loader

--- src/loaders.py
+++ src/loaders.py
@@ -1,6 +1,7 @@
 # I want AIden to be able to work with information from various sources
 import os
 from langchain.document_loaders import TextLoader
+from langchain.document_loaders.csv_loader import CSVLoader
 
 directory="./data/raw"
 
@@ -12,3 +13,13 @@ def load_txt_files(directory):
             loader = TextLoader(file_path)
             documents.extend(loader.load())
     return documents
+
+def load_csv_files(directory):
+    documents = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(directory, filename)
+            loader = CSVLoader(file_path)
+            documents.extend(loader.load())
+    return documents
+
