f6ca45f118a6fe32e04cad552762b1d23578a1b5
Antreas Christofi <victoryspecificationii@gmail.com>
2024-06-18 19:37:29 +0300

    added llama2 model, faster than mistral

---
+++ src/llama2_onboard_llm.py
@@ -0,0 +1,36 @@
+from langchain_community.llms import LlamaCpp
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from huggingface_hub import hf_hub_download
+
+
+def load_llm():
+    model_name = "TheBloke/Llama-2-7B-Chat-GGUF"
+    model_file = "llama-2-7b-chat.Q4_0.gguf"
+    model_path = hf_hub_download(model_name, filename=model_file)
+    return model_path
+
+# Load the LlamaCpp language model, adjust GPU usage based on your hardware
+llm = LlamaCpp(
+    model_path=load_llm(),
+    n_gpu_layers=0,
+    n_batch=512,  # Batch size for model processing
+    verbose=False,  # Enable detailed logging for debugging
+)
+
+# Define the prompt template with a placeholder for the question
+template = """
+Question: {question}
+
+Answer:
+"""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+# Create an LLMChain to manage interactions with the prompt and model
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+print("Chatbot initialized, ready to chat...")
+while True:
+    question = input("> ")
+    answer = llm_chain.run(question)
+    print(answer, '\n')
