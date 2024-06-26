4cedbe6c6abe10c987cbc7a03c8d4532aceb3b14
Antreas Christofi <victoryspecificationii@gmail.com>
2024-06-26 15:38:36 +0300

    added LLM startuo check

--- src/copilot.py
+++ src/copilot.py
@@ -2,8 +2,11 @@ import startup_functions
 import mistral_onboard_llm
 import llama2_onboard_llm
 
-startup_functions.print_banner()
+
 
 if __name__ == "__main__":
+    startup_functions.print_banner()
+    startup_functions.check_local_llm_availability()
+    
     print(mistral_onboard_llm.ask_question("Are you Mistral?"))
     print(llama2_onboard_llm.ask_question("Are you Llama2?"))
--- src/startup_functions.py
+++ src/startup_functions.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import openai
 import mistral_onboard_llm
+import llama2_onboard_llm
 
 def print_banner():
     ascii_art = """
@@ -15,3 +16,32 @@ def print_banner():
 
     """
     print(ascii_art)
+
+def check_local_llm_availability():
+    print("Startup Check: Checking LLM models in ~/.cache/huggingface/hub")
+    # Get the paths to the model files
+    mistral_path = mistral_onboard_llm.load_llm()
+    llama2_path = llama2_onboard_llm.load_llm()
+
+    # Check if the model files exist
+    if not os.path.isfile(mistral_path):
+        print(f"Error: Mistral model file not found at {mistral_path}. \n")
+        mistral_found = False
+    else:
+        print(f"Success: Mistral model file found at {mistral_path}. \n")
+        mistral_found = True
+
+    if not os.path.isfile(llama2_path):
+        print(f"Error: Llama2 model file not found at {llama2_path}. \n")
+        llama2_found = False
+    else:
+        print(f"Success: Llama2 model file found at {llama2_path}. \n")
+        llama2_found = True
+    
+    if (llama2_found or mistral_found):
+        return True
+    else:
+        print(f"LLM's not found in ~/.chache/huggingface/hub directory. Exiting...") 
+        exit(0)
+    
+
