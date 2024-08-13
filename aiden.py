3376cf151e5cd04b29a9bfb1e821a5092e5770b1
Antreas Christofi <victoryspecificationii@gmail.com>
2024-08-13 17:48:36 +0300

    added logger to api

--- aiden.py
+++ aiden.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import logging
 
 api = FastAPI()
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 @api.get("/")
 async def root():
+    logger.info("root point accessed")
     return {"message": "Welcome to AIden's API server."}
