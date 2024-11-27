e03d7c5937d5cf69eb6cb4eddb7bc508908b467a
Antreas Christofi <victoryspecificationii@gmail.com>
2024-11-27 14:40:48 +0200

    rm data

--- src/api/v1/aiden/aiden.py
+++ src/api/v1/aiden/aiden.py
@@ -8,16 +8,6 @@ from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 import telemetry
 import asyncio
 
-# llm stuff
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.llms.llama_cpp import LlamaCPP
-from llama_index.llms.llama_cpp.llama_utils import (
-    messages_to_prompt,
-    completion_to_prompt,
-)
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     app.state.llm_power = "default_model"
@@ -62,51 +52,3 @@ async def telemetry_middleware(request: Request, call_next):
 @app.get("/")
 async def root():
     return {"message": "Welcome to Aiden's API server."}
-
-# @api.get("/embedding")
-# async def embed():
-#     # loads BAAI/bge-small-en-v1.5
-#     local_embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
-
-#     data = SimpleDirectoryReader(input_dir="./data/paul_graham/").load_data()
-#     index = VectorStoreIndex.from_documents(data, embed_model = local_embedding_model, show_progress = True)
-
-@app.get("/generate")
-async def generate():
-
-    # Determine the model URL based on the app's state
-    if app.state.llm_power == "default_model":
-        model_url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf"
-    elif app.state.llm_power == "edge_device_model":
-        model_url = "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q2_K.gguf"
-    else:
-        raise HTTPException(status_code=400, detail="Invalid model selection")
-
-    llm = LlamaCPP(
-        # You can pass in the URL to a GGML model to download it automatically
-        model_url=model_url,
-        # optionally, you can set the path to a pre-downloaded model instead of model_url
-        model_path=None,
-        temperature=0.1,
-        max_new_tokens=256,
-        # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
-        context_window=3900,
-        # kwargs to pass to __call__()
-        generate_kwargs={},
-        # kwargs to pass to __init__()
-        # set to at least 1 to use GPU
-        model_kwargs={"n_gpu_layers": 0},
-        # transform inputs into Llama2 format
-        messages_to_prompt=messages_to_prompt,
-        completion_to_prompt=completion_to_prompt,
-        verbose=True,
-    )
-
-    # response = llm.complete("Hello! Can you tell me a poem about cats and dogs?")
-    # print(response.text)
-
-    response_iter = await asyncio.to_thread(llm.stream_complete, "What is the meaning of life?")
-    for response in response_iter:
-        print(response.delta, end="", flush=True)
-    print("done")
-    return response
