b7fd44dc16480819fd56cb6d827f4f6feef310f6
Antreas Christofi <victoryspecificationii@gmail.com>
2025-04-23 22:37:34 +0300

    containerized api and added compose entry

--- src/aiden/aiden.py
+++ src/aiden/aiden.py
@@ -10,7 +10,7 @@ def home():
 
 @app.get('/ask')
 def ask(prompt :str):
-    res = requests.post('http://localhost:11434/api/generate', json={
+    res = requests.post('http://ollama-cpu:11434/api/generate', json={
         "prompt": prompt,
         "stream" : False,
         "model" : "smollm:135m"
