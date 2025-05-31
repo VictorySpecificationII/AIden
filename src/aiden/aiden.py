07ae3b0241d5b9028b747ae620780dc91ae1a0f2
VictorySpecificationII <antreas@christofi.me>
2025-05-31 15:34:28 +0300

    modified call to container in api to ollama from ollama-cpu

--- src/aiden/aiden.py
+++ src/aiden/aiden.py
@@ -128,7 +128,7 @@ def home():
 @app.get('/ask')
 def ask(prompt: str):
     try:
-        res = requests.post('http://ollama-cpu:11434/api/generate', json={
+        res = requests.post('http://ollama:11434/api/generate', json={
             "prompt": prompt,
             "stream": False,
             "model": "smollm:135m"
